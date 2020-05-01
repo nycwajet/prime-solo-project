79fe961d55bfe21e2cac6b395b815b68338839a1
Henry Marin <henrymarin@rocketmail.com>
2020-05-01 09:43:42 -0500

    added get route

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const rejectUnauthenticated = require('../modules/authentication-middleware');
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-    
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('req.user:', req.user);
+    pool.query('SELECT * FROM "user" WHERE id=$1;', [req.user.id])
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
 });
 
 /**
