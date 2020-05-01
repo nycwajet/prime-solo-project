ff22cd9e99cbf6fe9e2690eeb9a77c5e746d3812
Henry Marin <henrymarin@rocketmail.com>
2020-05-01 12:37:46 -0500

    added components for documents

---
+++ src/components/AddDocument/AddDocument.js
@@ -0,0 +1,11 @@
+
+//import react
+
+//state
+
+//variables
+
+
+// addDocument form
+//onsubmit
+//onclick
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -11,17 +11,17 @@ const UserPage = ({user}) => (
     <h1 id="welcome">
       Welcome, { user.username }!
     </h1>
+    {/* add user infor ie, first lastname, address */}
     <p>Your ID is: {user.id}</p>
-    
+    {/* Add table to display information */}
+    {/* addDocument button */}
   </div>
 );
 
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
-// const mapStateToProps = ({user}) => ({ user });
-const mapStateToProps = state => ({
-  user: state.user,
-});
+
+const mapStateToProps = ({user}) => ({ user });
 
 // this allows us to use <App /> in index.js
 export default connect(mapStateToProps)(UserPage);
